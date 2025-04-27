440d9c75f95fb6ba6fbbd4293c0bb58d0f613cd9
sonpalomo <sonpalomo@alumnos.cei.es>
2025-04-27 23:21:37 +0200

    .

--- js/script.js
+++ js/script.js
@@ -10,3 +10,22 @@ menuBtn.addEventListener("click", () => {
     }
 });
 
+
+const bloques = document.querySelectorAll(".Bloque-content");
+const Title = document.querySelectorAll(".Bloque-title");
+
+//Cuando CLICK en h2,
+  //QUITAR la clase activo de todos los bloques
+  //Vamos a añadir la clase activo al bloque con la posición del title
+
+  Title.forEach( ( cadaTitle , i )=>{
+    cadaTitle.addEventListener("click", ()=> {
+
+        bloques.forEach( (cadaBloque , i ) => {
+            cadaBloque.classList.remove("active");
+        });
+            bloques[i].classList.add("active");
+    });
+});
+
+
