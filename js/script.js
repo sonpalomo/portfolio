0183860c4ed5855081235537ac5cb75929cd953b
sonpalomo <sonpalomo@alumnos.cei.es>
2025-07-26 12:45:29 +0200

    .

--- js/script.js
+++ js/script.js
@@ -32,14 +32,21 @@ Title.forEach((cadaTitle, i) => {
 
 //header blur visible al hacer scroll, if not is hidden
 const header = document.querySelector(".Header");
+//header blur siempre visible en layoutproject.html
+const isLayoutProject = document.body.dataset.page === "layoutproject";
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 5) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
+if (isLayoutProject) {
+  header.classList.add("scrolled");
+} else {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 5) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+
+}
 
 
 // Buscar layoutproject x correspondiente
@@ -47,71 +54,73 @@ window.addEventListener("scroll", () => {
 const parametro = new URLSearchParams(window.location.search);
 const projectId = parametro.get("project");
 
+
 const projects = {
   "1": {
-    title: "Mesita Scheint",
+    title: "Mesa Scheint",
     intro: "Diseño de mobiliario organico",
-    images: ["./sources/images/scheint-1.png",
-      "./sources/images/scheint-2.png",
-      "./sources/images/scheint-3.png",
-      "./sources/images/scheint-4.png",
+    background: ["url(../sources/images/scheint-1.png)",
     ],
 
     info: "Este proyecto se inspira en el diseño organico"
   },
 
   "2": {
-    title: "LIA",
-    intro: "Identidad Visual - Branding para centro de cultura contemporánea",
-    images: ["./sources/images/scheint-.png",
-    
-    ],
-
-    info: "Este proyecto se inspira en el diseño organico"
-   },
-
-   "3": {
     title: "NEA",
-    intro: "Identidad Visual - Branding para centro de cultura contemporánea",
-    images: ["./sources/images/nea-featuredimage.webp",
-    
+    intro: "Diseño Gráfico + Diseño Web",
+    background: ["url(../sources/images/home-nea.webp)",
     ],
 
     info: "Este proyecto se inspira en el diseño organico"
-   },
-   
-  };
-
-  const project = projects[projectId];
-
-  if (project) {
-    document.getElementById("projectTitle").textContent = project.title;
-    document.getElementById("projectIntro").textContent = project.intro;
-    document.getElementById("projectInfo").textContent= project.info;
-
-    //Carrusel de imagenes de proyectos
-
-    const carousel =document.getElementById("projectCarousel");
-
-    project.images.forEach(src =>{
-      const img =document.createElement("img");
-      img.src =src;
-      img.alt = project.title;
-      img.className= "carousel-img";
-      carousel.appendChild (img);
-
-    })
+  },
 
 
 
-   
+};
 
-  } else {
-    document.getElementById("projectTitle").textContent = "Proyecto no encontrado";
-    document.getElementById("projectIntro").textContent = "";
-    document.getElementById("projectInfo").textContent = "Texto no encontrado";
+const images = {
+  "1": {
+    image: [
+      "../sources/images/scheint-1.png",
+      "../sources/images/scheint-2.png",
+      "../sources/images/scheint-3.png"
+    ]
+  },
 
+  "2": {
+    image: [
+      "../sources/images/scheint-1.png",
+      "../sources/images/scheint-2.png",
+      "../sources/images/scheint-3.png"
+    ]
   }
+};
+
+
+
+const project = projects[projectId];
+
+if (project) {
+  document.getElementById("projectTitle").textContent = project.title;
+  document.getElementById("projectIntro").textContent = project.intro;
+  document.getElementById("projectInfo").textContent = project.info;
+
+
+
+  // document.getElementById("projectFeatured").style.backgroundImage= project.background [0];
+
+  const featured = document.getElementById("projectFeatured");
+  featured.style.backgroundImage = project.background[0];
+  featured.style.backgroundSize = "cover";
+  featured.style.backgroundRepeat = "no-repeat";
+  featured.style.backgroundPosition = "center";
+
+} else {
+  document.getElementById("projectTitle").textContent = "Proyecto no encontrado";
+  document.getElementById("projectIntro").textContent = "Texto no encontrado";
+  document.getElementById("projectInfo").textContent = "Texto no encontrado";
+
+}
 
 
 
